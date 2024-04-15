6658676f10ad23eb30db36f9f822f9055dec944c
Lee Sumin <survd0404@gmail.com>
2024-04-15 14:55:57 +0900

    feign client test

--- src/main/java/com/app/jwtpractice/api/health/dto/HealthCheckResponseDto.java
+++ src/main/java/com/app/jwtpractice/api/health/dto/HealthCheckResponseDto.java
@@ -1,12 +1,16 @@
 package com.app.jwtpractice.api.health.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HealthCheckResponseDto {
     private String health;
     private List<String> activeProfiles;
