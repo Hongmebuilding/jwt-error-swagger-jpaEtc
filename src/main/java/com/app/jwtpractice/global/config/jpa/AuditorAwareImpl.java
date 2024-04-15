cd28a3b1bd3515a0de11c8d06e79cfa87ffa0b2f
Lee Sumin <survd0404@gmail.com>
2024-04-15 11:11:28 +0900

    jpa audit config setting

---
+++ src/main/java/com/app/jwtpractice/global/config/jpa/AuditorAwareImpl.java
@@ -0,0 +1,25 @@
+package com.app.jwtpractice.global.config.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        String modifiedBy = httpServletRequest.getRequestURI();
+        if (!StringUtils.hasText(modifiedBy)) {
+            modifiedBy = "unknown";
+        }
+        return Optional.of(modifiedBy);
+    }
+}
